87997c6cd1578c07e27602f13e9d8bb9799ee3a0
Prajjwal Sinha <sinhaprajjwal009@gmail.com>
2023-04-24 22:48:24 +0530

    Initial Commit

---
+++ SprintOn/SprintOnApp.swift
@@ -0,0 +1,17 @@
+//
+//  SprintOnApp.swift
+//  SprintOn
+//
+//  Created by Niraj Kumar on 24/04/23.
+//
+
+import SwiftUI
+
+@main
+struct SprintOnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
